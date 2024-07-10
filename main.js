150139a0f5748d4b708562b6acbaa19bf521caf3
M1rzebeyM1rzebeyli <mirzemurad88888@gmai.com>
2024-07-10 15:19:50 +0400

    V2

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
-app.listen(5000)
+app.listen(8000)
 app.get("/", (req, res) => {
     res.send("Hi World")
+})
+app.get("/asdfg", (req, res) => {
+    res.send("pgefghdfoguhsdofguhfgouHIUFHIGUHFIGH")
 })
